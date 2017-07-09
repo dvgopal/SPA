75d90d72c90355b025037efb96c159c1e50d0e21
Venugopal <dvgopal@gmail.com>
2017-07-09 01:05:30 -0500

    Working Solution

---
+++ Assignment3/js/app.js
@@ -0,0 +1,115 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems',FoundItemsDirective)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+
+
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+      items: '<',
+      onRemove: '&'
+    },
+    controller: FoundItemsDirectiveController,
+    controllerAs: 'menu',
+    bindToController: true
+  };
+
+  return ddo;
+}
+
+function FoundItemsDirectiveController() {
+  var menu = this;
+
+  menu.isFoundItems = function () {
+      if (menu.items.length > 0) {
+        return true;
+      }
+    return false;
+  };
+
+}
+
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var menu = this;
+
+  menu.searchTerm = "";
+  menu.searchItems = function () {
+    var promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
+      promise.then(function (response) {
+        console.log(response);
+        menu.found = response;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  menu.removeItem = function (itemIndex) {
+    // this.lastRemoved = "Last item removed was " + this.items[itemIndex].name;
+    MenuSearchService.removeItem(itemIndex);
+    // this.title = origTitle + " (" + list.items.length + " items )";
+    menu.found = MenuSearchService.getFoundItems();
+  };
+
+  menu.getErrorMessage = function(){
+    return MenuSearchService.getErrorMessage();
+  }
+}
+
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath) {
+  var service = this;
+
+  service.foundItems=[];
+  service.getMatchedMenuItems = function (shortName) {
+  service.errorMessage = "";
+    if(shortName == "" ){
+      service.errorMessage = "Please Enter Search String";
+      // return service.foundItems;
+    }
+    return  $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json"),
+
+    }).then (function(result) {
+      // Process the result.
+      console.log(result);
+      var items = result.data.menu_items;
+
+      if(shortName.length > 0){
+        for (var i = 0; i < items.length; i++) {
+          var name = items[i].name;
+          if (name.toLowerCase().indexOf(shortName) !== -1) {
+            service.foundItems.push(items[i]);
+          }
+        }
+      }
+      return service.foundItems;
+    });
+
+  };
+
+  service.removeItem = function (itemIndex) {
+    service.foundItems.splice(itemIndex, 1);
+  };
+
+  service.getFoundItems = function(){
+    return service.foundItems;
+  }
+
+  service.getErrorMessage = function(){
+    return service.errorMessage;
+  }
+
+}
+
+})();
